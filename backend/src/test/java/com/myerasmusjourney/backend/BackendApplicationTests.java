352b58692f30f7634f98ab69668ad834a0269b67
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2026-06-25 11:17:42 +0200

    Adding h2 database for tests

--- backend/src/test/java/com/myerasmusjourney/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/myerasmusjourney/backend/BackendApplicationTests.java
@@ -3,15 +3,11 @@ package com.myerasmusjourney.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
-        assertFalse(false);
     }
 
 }
