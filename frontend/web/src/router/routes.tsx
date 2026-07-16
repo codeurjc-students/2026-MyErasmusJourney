2a4faf92dd6857ed9fd567d4cf16cccd23d910bd
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2026-07-16 19:06:57 +0200

    inserting /experiences route and changing components in router

--- frontend/web/src/router/routes.tsx
+++ frontend/web/src/router/routes.tsx
@@ -1,9 +1,14 @@
 import { ROUTES } from "@shared/constants/routes";
 
 import ExperiencesPage from "../pages/ExperiencesPage";
+import HomePage from "../pages/HomePage/HomePage";
 export const routes = [
   {
     path: ROUTES.HOME,
+    element: <HomePage />
+  },
+  {
+    path: ROUTES.EXPERIENCES,
     element: <ExperiencesPage />
   }
 ];
