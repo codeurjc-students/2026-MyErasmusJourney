110b5eedd8e748952b0d5b85f380d0a50815400e
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2026-07-17 12:06:56 +0200

    setting general styles for web

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,10 +1,9 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header/Header.tsx';
 
 
 function App() {
-  console.log("Entrando en App...")
   return (
     <>
       <Header/>
