7c7f6fe13ff79491dc4ce4b4d38ae4a5a47561de
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2026-07-18 10:48:39 +0200

    adding about us page route to router.

--- frontend/shared/src/constants/routes.ts
+++ frontend/shared/src/constants/routes.ts
@@ -1,4 +1,5 @@
 export const ROUTES = {
     HOME: "/",
+    ABOUT_US: "/about-us",
     EXPERIENCES: "/experiences"
 };
--- frontend/web/src/router/routes.tsx
+++ frontend/web/src/router/routes.tsx
@@ -2,10 +2,15 @@ import { ROUTES } from "@shared/constants/routes";
 
 import ExperiencesPage from "../pages/ExperiencesPage";
 import HomePage from "../pages/HomePage/HomePage";
+import AboutUsPage from "src/pages/AboutUsPage/AboutUsPage";
 export const routes = [
   {
     path: ROUTES.HOME,
     element: <HomePage />
+  },
+  {
+    path: ROUTES.ABOUT_US,
+    element: <AboutUsPage />
   },
   {
     path: ROUTES.EXPERIENCES,
