4bb630c8a54f3bc80a41bb8fc8460ee335f8cf0e
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2026-07-24 13:10:40 +0200

    Adding getInfo request to userService

--- frontend/shared/src/services/user.service.ts
+++ frontend/shared/src/services/user.service.ts
@@ -5,12 +5,24 @@ export type UserService = ReturnType<typeof createUserService>;
 
 export function createUserService(api:ApiClient) {
   return {
-    signUp: (body: UserFormDTO) => signUp(api, body)
+    signUp: (body: UserFormDTO) => signUp(api, body),
+    getUserInfo: () => getUserInfo(api)
   };
 }
 
 async function signUp(api: ApiClient, body:UserFormDTO){
     const response = await api.post("/users/", body)
+
+    if (!response.ok){
+      const error = await response.text();
+      throw new Error(error);
+    }
+
+    return response.json();
+}
+
+async function getUserInfo(api:ApiClient) {
+  const response = await api.get("/users/me")
 
     if (!response.ok){
       const error = await response.text();
