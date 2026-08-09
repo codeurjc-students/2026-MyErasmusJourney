b25ba1d4a3d0f3760073d8d774e0f833873bda7f
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2026-08-09 18:43:06 +0200

    created new authenticated test for selenium

---
+++ backend/src/test/java/com/myerasmusjourney/backend/system/AuthenticatedSeleniumTest.java
@@ -0,0 +1,49 @@
+package com.myerasmusjourney.backend.system;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class AuthenticatedSeleniumTest extends BaseSeleniumTest{
+
+    @Override
+    @BeforeEach
+    void setUpWebDriver(){
+        startWebDriver();
+        driver.get("http://localhost:" + FRONTEND_PORT);
+        authenticateTest();
+    }
+
+    private void authenticateTest(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.linkText("Log in")
+        ));
+
+        WebElement linkToLogIn = driver.findElement(By.linkText("Log in"));
+
+        linkToLogIn.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.id("email")
+        ));
+
+        WebElement email = driver.findElement(By.id("email"));
+        WebElement password = driver.findElement(By.id("password"));
+
+        email.sendKeys("test@email.com");
+        password.sendKeys("password");
+
+        WebElement button = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/form/button"));
+        button.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.id("profileTitle")
+        ));
+    }
+}
