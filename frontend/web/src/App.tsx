5175e8ad49acb52d6b76d383a1d2a73d2c1cd88d
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2026-08-14 19:44:28 +0200

    adding hook to app to obtain user information if authenticated

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,9 +1,32 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header.tsx';
+import { useEffect } from 'react';
+import { useUserStore } from '@shared/stores/userStore.ts';
+import { createUserService } from '@shared/services/user.service.ts';
+import { API } from './api/client.ts';
+import type { userServiceProps } from '@shared/interfaces/userServiceProps.ts';
 
 
-function App() {
+function App({userService = createUserService(API)}: userServiceProps) {
+
+  const {setUser } = useUserStore();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      
+      try {
+        console.log("Fetching user info in app");
+        const data = await userService.getUserInfo();
+        setUser(data);
+      } catch {
+        setUser(null);
+      }
+    }
+
+    fetchUser();
+  },[])
+
   return (
     <>
       <Header/>
