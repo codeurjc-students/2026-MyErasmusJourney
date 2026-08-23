f66626147fe2f71474b70faa6713862e239c986a
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2026-08-23 11:47:26 +0200

    E2E tests for delete user by id

--- backend/src/test/java/com/myerasmusjourney/backend/e2e/UsersTest.java
+++ backend/src/test/java/com/myerasmusjourney/backend/e2e/UsersTest.java
@@ -1,5 +1,8 @@
 package com.myerasmusjourney.backend.e2e;
 
+import com.myerasmusjourney.backend.dto.UserDTO;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Tag;
@@ -11,11 +14,52 @@ import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.fail;
 
 @Tag("e2e")
 public class UsersTest extends AuthenticatedE2ETest {
 
     private static final Logger log = LoggerFactory.getLogger(UsersTest.class);
+
+    private static Long deletedUserId;
+
+    private void createUserToDelete() throws JSONException {
+        JSONObject body = new JSONObject();
+        body.put("email","deletedUser@gmail.com");
+        body.put("displayName", "test");
+        body.put("fullName", "testUser");
+        body.put("city", "Munich");
+        body.put("country", "Germany");
+        body.put("password", "password");
+        body.put("passwordConfirmation", "password");
+
+        Response response = given()
+                .contentType("application/json")
+                .body(body.toString()).
+                when()
+                .post("/api/v1/users/");
+
+        ResponseBody responseBody = response.body();
+        if(response.statusCode() == 201){
+            UserDTO userDTO = responseBody.as(UserDTO.class);
+            deletedUserId = userDTO.id();
+        }
+    }
+
+    private void authenticateAsDeletedUser() throws JSONException {
+        JSONObject body = new JSONObject();
+        body.put("username","deletedUser@gmail.com");
+        body.put("password", "password");
+
+        Response response =
+                given()
+                        .contentType("application/json")
+                        .body(body.toString()).
+                        when()
+                        .post("/api/v1/auth/login");
+
+        this.token = response.getCookie("AuthToken");
+    }
 
     @Test
     void testSuccessfulCreateUser() throws JSONException {
@@ -212,5 +256,96 @@ public class UsersTest extends AuthenticatedE2ETest {
                 .get("/api/v1/users/1")
                 .then()
                 .statusCode(403);
+    }
+
+    @Test
+    void testDeleteUserByIdSuccess(){
+        try{
+            createUserToDelete();
+            authenticateAsDeletedUser();
+            given()
+                    .cookie("AuthToken", this.token)
+                    .when()
+                    .delete("/api/v1/users/"+deletedUserId)
+                    .then()
+                    .statusCode(200)
+                    .contentType("application/json")
+                    .body("id", notNullValue())
+                    .body("id", equalTo(deletedUserId.intValue()))
+                    .body("fullName", equalTo("testUser"))
+                    .body("displayName", equalTo("test"))
+                    .body("email", equalTo("deletedUser@gmail.com"))
+                    .body("roles", equalTo(List.of("USER")));
+        } catch (JSONException e){
+            fail();
+        }
+    }
+
+    @Test
+    void testDeleteUserByIdNotFound(){
+        try{
+            obtainToken(true);
+            given()
+                    .cookie("AuthToken", this.token)
+                    .when()
+                    .delete("/api/v1/users/0")
+                    .then()
+                    .statusCode(404);
+        } catch (JSONException e){
+            fail();
+        }
+    }
+
+    @Test
+    void testDeleteUserByIdFail(){
+        try{
+            createUserToDelete();
+            obtainToken(false);
+            given()
+                    .cookie("AuthToken", this.token)
+                    .when()
+                    .delete("/api/v1/users/"+deletedUserId)
+                    .then()
+                    .statusCode(403);
+        } catch (JSONException e){
+            fail();
+        }
+    }
+
+    @Test
+    void testDeleteUserByAdmin(){
+        try{
+            createUserToDelete();
+            obtainToken(true);
+            given()
+                    .cookie("AuthToken", this.token)
+                    .when()
+                    .delete("/api/v1/users/"+deletedUserId)
+                    .then()
+                    .statusCode(200)
+                    .contentType("application/json")
+                    .body("id", notNullValue())
+                    .body("id", equalTo(deletedUserId.intValue()))
+                    .body("fullName", equalTo("testUser"))
+                    .body("displayName", equalTo("test"))
+                    .body("email", equalTo("deletedUser@gmail.com"))
+                    .body("roles", equalTo(List.of("USER")));
+        } catch (JSONException e){
+            fail();
+        }
+    }
+
+    @Test
+    void testDeleteUserByIdWithoutAuthentication(){
+        try{
+            createUserToDelete();
+            given()
+                    .when()
+                    .delete("/api/v1/users/"+deletedUserId)
+                    .then()
+                    .statusCode(401);
+        } catch (JSONException e){
+            fail();
+        }
     }
 }
