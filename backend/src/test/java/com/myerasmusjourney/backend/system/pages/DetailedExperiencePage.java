09910a9303012d3694e7fed5e68b7284fef6e3d9
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2026-08-28 19:58:44 +0200

    implementing comments selenium tests

--- backend/src/test/java/com/myerasmusjourney/backend/system/pages/DetailedExperiencePage.java
+++ backend/src/test/java/com/myerasmusjourney/backend/system/pages/DetailedExperiencePage.java
@@ -1,6 +1,6 @@
 package com.myerasmusjourney.backend.system.pages;
 
-import com.myerasmusjourney.backend.system.BaseSeleniumTest;
+import com.myerasmusjourney.backend.system.AuthenticatedSeleniumTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -9,18 +9,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertTrue;
 
 @Tag("system")
-public class DetailedExperiencePage extends BaseSeleniumTest {
+public class DetailedExperiencePage extends AuthenticatedSeleniumTest {
 
-    @Test
-    void testRendersData(){
+    private final WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    private void goToExperience(){
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.linkText("Experiences")
@@ -37,9 +34,46 @@ public class DetailedExperiencePage extends BaseSeleniumTest {
         WebElement linkToFirstExperience = driver.findElement(By.xpath("/html/body/div/div/div/main/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/a"));
 
         linkToFirstExperience.click();
+    }
+
+    @Test
+    void testRendersData(){
+
+        goToExperience();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("/html/body/div/div/div/main/div/div[2]/h3")
+        ));
+    }
+
+    @Test
+    void testPostAndRenderComments(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("/html/body/div/div/div/main/div/div[2]/h3")
+        ));
+
+        WebElement commentInput = driver.findElement(By.xpath("/html/body/div/div/div/aside/div[2]/input"));
+        WebElement postButton = driver.findElement(By.xpath("/html/body/div/div/div/aside/div[2]/button"));
+
+        commentInput.sendKeys("New comment");
+        postButton.click();
+
+        WebElement newComment = driver.findElement(By.xpath("/html/body/div/div/div/aside/div[1]/div/div[1]"));
+        assertTrue(newComment.getText().contains("New comment"));
+    }
+
+    @Test
+    void testLogInLink(){
+
+        logOutUser();
+
+        goToExperience();
+
+        WebElement signInLink = driver.findElement(By.linkText("Sign in"));
+        signInLink.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.id("email")
         ));
     }
 }
